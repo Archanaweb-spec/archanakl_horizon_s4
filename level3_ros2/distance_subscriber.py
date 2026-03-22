b98defbfce1db8a71037ac0449c1a307afc2e8cb
archanakl-79 <archanakl047@gmail.com>
2026-03-22 19:23:18 +0530

    Implement distance subscriber node in ROS2

---
+++ level3_ros2/distance_subscriber.py
@@ -0,0 +1,26 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int32
+
+class DistanceSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('distance_subscriber')
+        self.sub = self.create_subscription(
+            Int32,
+            '/distance',
+            self.callback,
+            10
+        )
+
+    def callback(self, msg):
+        self.get_logger().info('Received distance: %d' % msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = DistanceSubscriber()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
